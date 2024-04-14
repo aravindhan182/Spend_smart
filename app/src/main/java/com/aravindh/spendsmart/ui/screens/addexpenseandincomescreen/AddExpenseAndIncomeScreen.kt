654029f8553be00525d18632b834146a67871504
aravindh2106 <aravindhan@logicsoft.co.in>
2024-04-14 16:04:08 +0530

    transaction category dropdown added

--- app/src/main/java/com/aravindh/spendsmart/ui/screens/addexpenseandincomescreen/AddExpenseAndIncomeScreen.kt
+++ app/src/main/java/com/aravindh/spendsmart/ui/screens/addexpenseandincomescreen/AddExpenseAndIncomeScreen.kt
@@ -1,10 +1,21 @@
 package com.aravindh.spendsmart.ui.screens.addexpenseandincomescreen
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -12,12 +23,59 @@ import androidx.navigation.NavController
 
 @Composable
 fun AddExpenseAndIncomeScreen(navController: NavController) {
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        contentAlignment = Alignment.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("AddExpenseAndIncomeScreen")
+        Text(text = "Choose your category")
+        Spacer(modifier = Modifier.padding(5.dp))
+        DropDown()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDown() {
+    val list = listOf("INCOME", "EXPENSE")
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    var selectedText by remember {
+        mutableStateOf(list[0])
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = !isExpanded }
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.menuAnchor(),
+                value = selectedText,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+
+                })
+            ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                list.forEachIndexed { index, s ->
+                    DropdownMenuItem(
+                        text = { Text(text = s) },
+                        onClick = {
+                            selectedText = list[index]
+                            isExpanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                    )
+                }
+            }
+        }
     }
 }
